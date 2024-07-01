268bdfd3aad1b2525090bcbbe4702040af7b2da3
Alexey Mironov <alexeymironov1991@gmail.com>
2024-07-01 19:57:55 +0300

    feat: Добавить основу приложения

---
+++ src/main/java/ru/mironov/springaop/util/Constants.java
@@ -0,0 +1,8 @@
+package ru.mironov.springaop.util;
+
+import java.time.LocalDateTime;
+
+public class Constants {
+    public static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    public static final LocalDateTime MIN_DATE = LocalDateTime.of(2024, 1, 1, 0, 0, 0);
+}
