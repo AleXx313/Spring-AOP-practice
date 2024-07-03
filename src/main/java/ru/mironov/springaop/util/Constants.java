70dafe2992e8ee8cf8f6742d327066706c267ad0
Alexey Mironov <alexeymironov1991@gmail.com>
2024-07-03 22:30:35 +0300

    fix: fix Временная константа

--- src/main/java/ru/mironov/springaop/util/Constants.java
+++ src/main/java/ru/mironov/springaop/util/Constants.java
@@ -4,5 +4,5 @@ import java.time.LocalDateTime;
 
 public class Constants {
     public static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
-    public static final LocalDateTime MIN_DATE = LocalDateTime.of(2024, 1, 1, 0, 0, 0);
+    public static final LocalDateTime MIN_DATE = LocalDateTime.of(2024, 7, 1, 0, 0, 0);
 }
